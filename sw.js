40fd61c28040da36132197210d123236eb2d5e71
bazarisamanshexani-blip <bazarisamanshexani@gmail.com>
2026-04-16 22:39:33 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,20 @@
-const CACHE = 'bazari-v1';
+const CACHE = 'bazari-saman-v4';
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/'])));
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', e => {
+  if(e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
